a327d5202e42acbd7358f6fc3eeebcb4a490a923
eunbime <ebk1014@gmail.com>
2024-11-27 10:19:50 +0900

    feat: 메인 페이지 상품 목록 UI 생성

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,11 @@
+import { Product } from "@prisma/client";
 import getCurrentUser from "../actions/getCurrentUser";
 import getProducts, { ProductsParams } from "../actions/getProducts";
+
+import Container from "@/components/common/Container";
+import EmptyState from "@/components/EmptyState";
+import FloatingButton from "@/components/common/FloatingButton";
+import ProductCard from "@/components/products/ProductCard";
 
 interface HomeProps {
   searchParams: ProductsParams;
@@ -9,5 +15,35 @@ export default async function Home({ searchParams }: HomeProps) {
   const products = await getProducts(searchParams);
   const currentUser = await getCurrentUser();
 
-  return <main>everyone</main>;
+  console.log(products);
+
+  return (
+    <Container>
+      {/* Categories */}
+
+      {products?.data.length === 0 ? (
+        <EmptyState showReset />
+      ) : (
+        <>
+          <div
+            className="grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3
+          lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5"
+          >
+            {products?.data.map((product: Product) => (
+              <ProductCard
+                key={product.id}
+                data={product}
+                currentUser={currentUser}
+              />
+            ))}
+          </div>
+
+          {/* Pagination */}
+
+          {/* FloatingButton */}
+          <FloatingButton href="/products/upload">+</FloatingButton>
+        </>
+      )}
+    </Container>
+  );
 }
---
+++ src/components/EmptyState.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Button from "./common/Button";
+import Heading from "./common/Heading";
+
+interface EmptyStateProps {
+  title?: string;
+  subtitle?: string;
+  showReset?: boolean;
+}
+
+const EmptyState = ({
+  title = "일치하는 게 없습니다.",
+  showReset = "일부 필터를 변경하거나 제거해 보십시오,",
+  subtitle,
+}: EmptyStateProps) => {
+  const router = useRouter();
+
+  return (
+    <div className="h-[60vh] flex flex-col gap-2 justify-center items-center">
+      <Heading center title={title} subtitle={subtitle} />
+      <div className="w-48 mt-4">
+        {showReset && (
+          <Button
+            outline
+            label="모든 필터 제거"
+            onClick={() => router.push("/")}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default EmptyState;
