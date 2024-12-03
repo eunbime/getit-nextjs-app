1cf8dc2c2573108a213bdb993df7e2346da7014f
eunbime <ebk1014@gmail.com>
2024-12-03 21:52:43 +0900

    feat: ChatHeader 컴포넌트 구현

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { TUserWithChat } from "@/types/indes";
+import { TConversation, TUserWithChat } from "@/types/indes";
 import ChatHeader from "./ChatHeader";
 import Message from "./Message";
 import Input from "./Input";
@@ -14,6 +14,11 @@ interface ChatProps {
 }
 
 const Chat = ({ receiver, currentUser, setLayout }: ChatProps) => {
+  const conversation: TConversation | undefined =
+    currentUser?.conversations.find((conversation: TConversation) => {
+      return conversation.users.find((user) => user.id === receiver.receiverId);
+    });
+
   if (!receiver.receiverName || !currentUser) {
     return <div className="w-full h-full"></div>;
   }
@@ -21,7 +26,16 @@ const Chat = ({ receiver, currentUser, setLayout }: ChatProps) => {
   return (
     <div className="w-full">
       <div>
-        <ChatHeader />
+        <ChatHeader
+          setLayout={setLayout}
+          receiverImage={receiver.receiverImage}
+          receiverName={receiver.receiverName}
+          lastMessageTime={
+            conversation?.messages
+              .filter((message) => message.receiverId === currentUser.id)
+              .slice(-1)[0]?.createdAt
+          }
+        />
       </div>
 
       <div className="flex flex-col gap-8 overflow-auto h-[calc(100vh_-_60px_-_70px_-_80px)]">
