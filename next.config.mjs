c8e286a55aa80cd110282f779cea11badd9bbd20
eunbime <ebk1014@gmail.com>
2024-12-27 18:10:35 +0900

    fix: 배포 및 개발 환경에서 소켓 연결되도록 수정

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,12 @@ const nextConfig = {
     ],
   },
   webpack: (config) => {
-    config.externals = [...config.externals, "bcrypt", "socket.io-client"];
+    // socket.io-client를 externals에서 제거하고 fallback 설정 추가
+    config.externals = [...config.externals, "bcrypt"];
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      net: false,
+    };
     return config;
   },
   experimental: {
@@ -20,10 +25,15 @@ const nextConfig = {
   },
   // 웹소켓 연결을 위한 리라이트 설정
   async rewrites() {
+    const isDevelopment = process.env.NODE_ENV === "development";
+    const socketUrl = isDevelopment
+      ? "http://localhost:3000/api/socket" // 개발 환경
+      : "https://market-nextjs-app.vercel.app/api/socket"; // 배포 환경
+
     return [
       {
         source: "/api/socket/:path*",
-        destination: `${process.env.NEXT_PUBLIC_SITE_URL}/api/socket/:path*`,
+        destination: `${socketUrl}/:path*`,
       },
     ];
   },
