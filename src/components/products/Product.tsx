3533e603e327e65cf5b4096e76b2411b4f179a1f
eunbime <ebk1014@gmail.com>
2024-12-28 20:44:39 +0900

    fix: 스켈레톤 조건부 수정

--- src/components/products/Product.tsx
+++ src/components/products/Product.tsx
@@ -25,6 +25,8 @@ const Products: React.FC<ProductsProps> = ({ searchParams, currentUser }) => {
     },
   });
 
+  console.log(Object.keys(searchParams).length === 0);
+
   if (isLoading) {
     return (
       <div className="w-full grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
@@ -33,12 +35,25 @@ const Products: React.FC<ProductsProps> = ({ searchParams, currentUser }) => {
             key={index}
             className="flex flex-col space-y-3 justify-center items-center"
           >
-            <Skeleton className="md:h-[200px] h-[300px] md:w-[250px] w-[350px] rounded-xl" />
-            <div className="space-y-2">
-              <Skeleton className="md:h-4 h-6 md:w-[200px] w-[300px]" />
-              <Skeleton className="md:h-4 h-6 md:w-[150px] w-[250px]" />
-              <Skeleton className="md:h-4 h-6 md:w-[250px] w-[350px]" />
-            </div>
+            {Object.keys(searchParams).length === 0 ? (
+              <>
+                <Skeleton className="md:h-[250px] h-[300px] md:w-[250px] w-[350px] rounded-xl" />
+                <div className="space-y-2">
+                  <Skeleton className="md:h-4 h-6 md:w-[200px] w-[300px]" />
+                  <Skeleton className="md:h-4 h-6 md:w-[150px] w-[250px]" />
+                  <Skeleton className="md:h-4 h-6 md:w-[250px] w-[350px]" />
+                </div>
+              </>
+            ) : (
+              <>
+                <Skeleton className="md:h-[200px] h-[300px] md:w-[200px] w-[350px] rounded-xl" />
+                <div className="space-y-2">
+                  <Skeleton className="md:h-4 h-6 md:w-[150px] w-[300px]" />
+                  <Skeleton className="md:h-4 h-6 md:w-[100px] w-[250px]" />
+                  <Skeleton className="md:h-4 h-6 md:w-[200px] w-[350px]" />
+                </div>
+              </>
+            )}
           </div>
         ))}
       </div>
